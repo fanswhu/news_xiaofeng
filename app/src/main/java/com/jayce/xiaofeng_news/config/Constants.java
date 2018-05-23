198ff7eb0ffbcd29a27dffb376b0ab5cb1356fd8
jayce <2583211306@qq.com>
2018-05-23 10:32:32 +0800

    增加文件

---
+++ app/src/main/java/com/jayce/xiaofeng_news/config/Constants.java
@@ -0,0 +1,9 @@
+package com.jayce.xiaofeng_news.config;
+
+/**
+ * Created by jayce on 2018/5/23.
+ */
+
+public class Constants {
+    public  final  static  String url_base_gank = "http://gank.io/";
+}
